76f07bf9afa12c449cd399ff010debadf2f3305a
Darin Minamoto <darinm223@gmail.com>
2019-09-28 05:30:47 -0700

    Adds some more typeclasses

--- src/classes.rs
+++ src/classes.rs
@@ -1,18 +1,21 @@
-use crate::plug::{Plug, Unplug};
+use crate::plug::{Plug, Plug2, Unplug, Unplug2};
 
 pub trait Functor: Unplug + Plug<<Self as Unplug>::A> {
     fn map<B, F>(self, f: F) -> <Self as Plug<B>>::Out
     where
         Self: Plug<B>,
-        F: FnMut(<Self as Unplug>::A) -> B;
+        F: FnOnce(<Self as Unplug>::A) -> B;
 }
 
-pub trait Applicative: Functor {
-    fn pure(value: <Self as Unplug>::A) -> Self;
+pub trait Apply: Functor {
     fn ap<B, F>(self, f: <Self as Plug<F>>::Out) -> <Self as Plug<B>>::Out
     where
         Self: Plug<B> + Plug<F>,
         F: FnOnce(<Self as Unplug>::A) -> B;
+}
+
+pub trait Applicative: Apply {
+    fn pure(value: <Self as Unplug>::A) -> Self;
 }
 
 pub trait Monad: Applicative {
@@ -33,15 +36,43 @@ pub trait Monoid: Semigroup {
 }
 
 pub trait Foldable: Unplug + Plug<<Self as Unplug>::A> {
-    fn fold_right<B, F>(self, init: B, f: F) -> B
+    fn fold_left<B, F>(self, init: B, f: F) -> B
     where
-        F: FnOnce(<Self as Unplug>::A, B) -> B;
+        F: FnOnce(B, <Self as Unplug>::A) -> B;
 }
 
-pub trait Traverse: Unplug + Plug<<Self as Unplug>::A> {
+pub trait Traverse: Functor + Foldable {
     fn traverse<F, M, B>(self, f: F) -> <M as Plug<<Self as Plug<B>>::Out>>::Out
     where
         Self: Plug<B>,
         M: Plug<<Self as Plug<B>>::Out> + Plug<B> + Applicative,
         F: FnOnce(<Self as Unplug>::A) -> <M as Plug<B>>::Out;
+}
+
+pub trait Eq: Unplug + Plug<<Self as Unplug>::A> {
+    fn eq(a: <Self as Unplug>::A, b: <Self as Unplug>::A) -> bool;
+}
+
+pub trait Show: Unplug + Plug<<Self as Unplug>::A> {
+    fn show(a: <Self as Unplug>::A) -> String;
+}
+
+pub trait Contravariant: Unplug + Plug<<Self as Unplug>::A> {
+    fn contramap<B, F>(self, f: F) -> <Self as Plug<B>>::Out
+    where
+        Self: Plug<B>,
+        F: FnOnce(B) -> <Self as Unplug>::A;
+}
+
+pub trait Alternative: Applicative {
+    fn empty() -> Self;
+    fn combine(self, other: Self) -> Self;
+}
+
+pub trait Bifunctor: Unplug2 + Plug2<<Self as Unplug2>::A, <Self as Unplug2>::B> {
+    fn bimap<C, D, F, G>(self, f: F, g: G) -> <Self as Plug2<C, D>>::Out
+    where
+        Self: Plug2<C, D>,
+        F: FnOnce(<Self as Unplug2>::A) -> C,
+        G: FnOnce(<Self as Unplug2>::B) -> D;
 }
