6562122e64be6e8935f0690dd928afd79e7a964a
Darin Minamoto <darinm223@gmail.com>
2019-10-04 04:37:41 -0700

    Adds tests for Vec instances

--- src/vec.rs
+++ src/vec.rs
@@ -81,3 +81,68 @@ impl<A: Copy> Alternative for Vec<A> {
         self.into_iter().chain(other.into_iter()).collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::classes::*;
+
+    #[test]
+    fn test_functor() {
+        assert_eq!(vec![1, 2, 3].fmap(|a| a + 1), vec![2, 3, 4]);
+        assert_eq!(vec![].fmap(|a: i32| a + 1), vec![]);
+    }
+
+    #[test]
+    fn test_applicative() {
+        let plus_one: Box<dyn Fn(i32) -> i32> = Box::new(|a| a + 1);
+        let plus_two: Box<dyn Fn(i32) -> i32> = Box::new(|a| a + 2);
+        let plus_three: Box<dyn Fn(i32) -> i32> = Box::new(|a| a + 3);
+        assert_eq!(
+            vec![1, 2, 3].ap(vec![plus_one, plus_two, plus_three]),
+            vec![2, 3, 4, 3, 4, 5, 4, 5, 6]
+        );
+    }
+
+    #[test]
+    fn test_monad() {
+        assert_eq!(
+            vec![1, 2, 3].bind(|i| vec![2, 3, 4].bind(|j| vec![(i, j)])),
+            vec![
+                (1, 2),
+                (1, 3),
+                (1, 4),
+                (2, 2),
+                (2, 3),
+                (2, 4),
+                (3, 2),
+                (3, 3),
+                (3, 4)
+            ]
+        );
+    }
+
+    #[test]
+    fn test_monoid() {
+        assert_eq!(vec![1, 2, 3].combine(vec![4, 5, 6]), vec![1, 2, 3, 4, 5, 6]);
+        assert_eq!(Vec::<i32>::mempty(), vec![]);
+        assert_eq!(Vec::mempty().combine(vec![1]).combine(vec![2]), vec![1, 2]);
+    }
+
+    #[test]
+    fn test_foldable() {
+        assert_eq!(
+            vec![1, 2, 3, 4, 5].fold_left("".to_owned(), |acc, i| format!("{}{}", acc, i)),
+            "12345"
+        );
+        assert_eq!(
+            vec![1, 2, 3, 4, 5].fold_right("".to_owned(), |i, acc| format!("{}{}", acc, i)),
+            "54321"
+        );
+    }
+
+    #[test]
+    fn test_alternative() {
+        assert_eq!(vec![1, 2, 3].combine(vec![4, 5, 6]), vec![1, 2, 3, 4, 5, 6]);
+        assert_eq!(Vec::<i32>::empty(), vec![]);
+    }
+}
